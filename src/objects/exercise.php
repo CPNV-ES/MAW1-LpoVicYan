6e4d06788523a23f9d05ec36082e78c30731c918
Yann Menoud <111496797+menoudyann@users.noreply.github.com>
2022-09-09 10:18:04 +0200

    T6 Implement class exercise.php in /objects

--- src/objects/exercise.php
+++ src/objects/exercise.php
@@ -7,10 +7,23 @@
  */
 
 
- /**
+/**
  * Contructor of Exercise
  */
-class Exercise {
+class Exercise
+{
 
-  
+  public $title;
+  public $creation_date;
+  public $modification_date;
+  public $status;
+
+
+  public function __construct($title, $creation_date, $modification_date, $status)
+  {
+    $this->title = $title;
+    $this->creation_date = $creation_date;
+    $this->modification_date = $modification_date;
+    $this->status = $status;
   }
+}
