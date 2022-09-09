201674202bee86257d6f04d81b74ed09a2136426
VicMtn <victorien.montavon@cpnv.ch>
2022-09-09 10:18:07 +0200

    T13 fix logo

--- src/views/exercises/create_questions.php
+++ src/views/exercises/create_questions.php
@@ -9,7 +9,7 @@
 
 <header class="heading managing">
     <section class="container">
-        <a href="/"><img src="/assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png" /></a>
+        <a href="/"><img src="/images/logo.png" /></a>
         <span class="exercise-label">Exercise: <a href="/exercises/750/fields">TestVM</a></span>
     </section>
 </header>
