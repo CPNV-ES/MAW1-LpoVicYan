ae475087a2ba834f5f99116a09415b24df0f335f
LPOdev <lpodevco@gmail.com>
2022-09-29 22:59:02 +0200

    T42 creation of Question class

--- src/models/question.php
+++ src/models/question.php
@@ -1,16 +1,34 @@
 <?php
-
 /**
- * Title: index
- * Author: MenoudY
- * Version: 1.0 from 29th August 2022
+ * Title: question
+ * Author: Luís Pedro Pinheiro
+ * Version: 1.0 from 29th September 2022
  */
 
-
-/**
- * Contructor of Question
- */
 class Question
 {
-    // code goes here...
+    protected $id;
+    protected $name;
+    protected $type;
+    protected $order;
+    protected $exercise_id;
+
+    public function __constructor($id, $name, $type, $order, $exercise_id)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->type = $type;
+        $this->order = $order;
+        $this->exercise_id = $exercise_id;
+    }
+
+    public static function loadById($id)
+    {
+        // loads a question from database using id
+    }
+
+    public static function getAll()
+    {
+        // returns an array with all the questions
+    }
 }
