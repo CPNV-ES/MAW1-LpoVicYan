3fcc97a0c6c4e5660054d64449837a2c0e589f35
LPOdev <lpodevco@gmail.com>
2022-09-29 22:27:12 +0200

    T33 Correction of database script

--- conception/exercicelooper.sql
+++ conception/exercicelooper.sql
@@ -1,57 +1,68 @@
--- MySQL Workbench Synchronization
--- Generated: 2022-09-16 08:36
--- Model: New Model
--- Version: 1.0
--- Project: Name of the project
--- Author: Luís Pedro Pinheiro
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE SCHEMA IF NOT EXISTS `exerciselooper` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema exerciselooper
+-- -----------------------------------------------------
 
+-- -----------------------------------------------------
+-- Schema exerciselooper
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `exerciselooper` DEFAULT CHARACTER SET utf8 ;
+USE `exerciselooper` ;
+
+-- -----------------------------------------------------
+-- Table `exerciselooper`.`exercises`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `exerciselooper`.`exercises` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idexercises_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `exerciselooper`.`questions`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `exerciselooper`.`questions` (
-  `idquestions` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
   `type` VARCHAR(45) NOT NULL,
-  `order` INT(11) NOT NULL,
-  `exercises_idexercises` INT(11) NOT NULL,
-  PRIMARY KEY (`idquestions`),
-  UNIQUE INDEX `idquestions_UNIQUE` (`idquestions` ASC) VISIBLE,
-  INDEX `fk_questions_exercises1_idx` (`exercises_idexercises` ASC) VISIBLE,
+  `order` INT NOT NULL,
+  `exercises_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idquestions_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `fk_questions_exercises1_idx` (`exercises_id` ASC) VISIBLE,
   CONSTRAINT `fk_questions_exercises1`
-    FOREIGN KEY (`exercises_idexercises`)
-    REFERENCES `exerciselooper`.`exercises` (`idexercises`)
+    FOREIGN KEY (`exercises_id`)
+    REFERENCES `exerciselooper`.`exercises` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `exerciselooper`.`exercises` (
-  `idexercises` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  `state` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idexercises`),
-  UNIQUE INDEX `idexercises_UNIQUE` (`idexercises` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
 
+-- -----------------------------------------------------
+-- Table `exerciselooper`.`answers`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `exerciselooper`.`answers` (
-  `idanswers` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `date` DATETIME NOT NULL,
   `answer` VARCHAR(255) NOT NULL,
-  `questions_idquestions` INT(11) NOT NULL,
-  PRIMARY KEY (`idanswers`),
-  INDEX `fk_answers_questions_idx` (`questions_idquestions` ASC) VISIBLE,
-  UNIQUE INDEX `idanswers_UNIQUE` (`idanswers` ASC) VISIBLE,
+  `questions_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_answers_questions_idx` (`questions_id` ASC) VISIBLE,
+  UNIQUE INDEX `idanswers_UNIQUE` (`id` ASC) VISIBLE,
   CONSTRAINT `fk_answers_questions`
-    FOREIGN KEY (`questions_idquestions`)
-    REFERENCES `exerciselooper`.`questions` (`idquestions`)
+    FOREIGN KEY (`questions_id`)
+    REFERENCES `exerciselooper`.`questions` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
