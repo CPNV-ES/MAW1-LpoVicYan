9d08e9adc792c0382897a5c1c603ea82a55a2a6e
Yann Menoud <Yann.MENOUD@cpnv.ch>
2022-10-11 19:31:06 +0200

    T46 - Structure of refactored database file

---
+++ src/models/Database.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Database.php
+ * MAW 1.1 
+ * Creator : Yann Menoud
+ * Creation date 11.10.2022
+ */
+require_once 'const.php';
+
+class Database
+{
+    protected $connection;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function query($query)
+    {
+
+    }
+
+    public static function close()
+    {
+
+    }
+
+    public function insert($table, $data)
+    {
+
+    }
+
+    public function update($table, $data)
+    {
+
+    }
+
+    public function delete($table, $data)
+    {
+
+    }
+
+
+
+
+}
