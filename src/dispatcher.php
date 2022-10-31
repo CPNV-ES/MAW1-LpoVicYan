77f21433c11d9cdf568e398080627f20263d0702
LPOdev <lpodevco@gmail.com>
2022-10-31 11:21:54 +0100

    T48 fix question model

--- src/dispatcher.php
+++ src/dispatcher.php
@@ -6,48 +6,83 @@
  * Version: 1.0 from 7th September 2022
  */
 
-function dispatch($bag)
+function dispatch( $bag )
 {
     $matches = [];
 
     //-----------------------------------------------------------------------------
-    if (preg_match('/^\/?$/', $bag['route'])) {
+    if ( preg_match( '/^\/?$/', $bag['route'] ) )
+    {
         $bag['view'] = 'views/site/index';
     }
+
     //-----------------------------------------------------------------------------
-    elseif (preg_match('/^\/exercises\/(.+)$/', $bag['route'], $matches)) {
-        $bag['handler'] = 'controllers/exercises/delete_exercise';
-        $bag['id'] = $matches[1];
+
+    elseif ( preg_match( '/^\/exercises\/(\w+)\/fields$/', $bag['route'], $matches ) )
+    {
+        $bag['post_data'] = [];
+
+        if ( $_SERVER['REQUEST_METHOD'] === 'POST' )
+        {
+            $bag['handler']     = 'controllers/exercises/modify';
+            $bag['exercise_id'] = $matches[1];
+            $bag['post_data']   = $_POST['field'];
+            var_dump( $bag['post_data'] );
+        }
+        else
+        {
+            $bag['handler']     = 'controllers/exercises/modify';
+            $bag['exercise_id'] = $matches[1];
+        }
+
     }
     //-----------------------------------------------------------------------------
-    elseif (preg_match('/^\/manage-exercises$/', $bag['route'], $matches)) {
+
+    elseif ( preg_match( '/^\/exercises\/(.+)$/', $bag['route'], $matches ) )
+    {
+        $bag['handler'] = 'controllers/exercises/delete_exercise';
+        $bag['id']      = $matches[1];
+    }
+    //-----------------------------------------------------------------------------
+
+    elseif ( preg_match( '/^\/manage-exercises$/', $bag['route'], $matches ) )
+    {
         $bag['handler'] = 'controllers/exercises/index';
     }
     //-----------------------------------------------------------------------------
-    elseif (preg_match('/^\/exercises\/create_exercise$/', $bag['route'], $matches)) {
+
+    elseif ( preg_match( '/^\/exercises\/create_exercise$/', $bag['route'], $matches ) )
+    {
         $bag['view'] = 'views/exercises/create_exercise';
-        
-        
+
     }
     //-----------------------------------------------------------------------------
-    elseif (preg_match('/^\/exercises\/answering$/', $bag['route'], $matches)) {
+
+    elseif ( preg_match( '/^\/exercises\/answering$/', $bag['route'], $matches ) )
+    {
         $bag['view'] = 'views/exercises/take_exercice';
     }
-        //-----------------------------------------------------------------------------
-    elseif (preg_match('/^\/exercises\/(.+)\/create_questions$/', $bag['route'], $matches)) {
+    //-----------------------------------------------------------------------------
+
+    elseif ( preg_match( '/^\/exercises\/(.+)\/create_questions$/', $bag['route'], $matches ) )
+    {
         $bag['post_exercise'] = $_POST['exercise_title'];
         $bag['post_question'] = $_POST['field'];
-        
-        if  ($bag['post_exercise']){
+
+        if ( $bag['post_exercise'] )
+        {
             $bag['handler'] = 'controllers/exercises/create_exercises';
         }
-        elseif ($bag['post_question']){
+        elseif ( $bag['post_question'] )
+        {
             $bag['handler'] = 'controllers/questions/index';
         }
-            $bag['view'] = 'views/exercises/create_questions';           
-        }
+        $bag['view'] = 'views/exercises/create_questions';
+    }
     //-----------------------------------------------------------------------------
-    else {
+
+    else
+    {
         $bag['status_code'] = 404;
     }
 
@@ -56,6 +91,7 @@ function dispatch($bag)
 
 //=============================================================================
 // Return the URL for the given named route (the opposite of the dispatcher)
-function route($name) {
-    return '/'.$name;
+function route( $name )
+{
+    return '/' . $name;
 }
--- src/models/question.php
+++ src/models/question.php
@@ -10,23 +10,23 @@ require_once '.const.php';
 
 class Question
 {
+    protected $exercise_id;
+
     protected $id;
 
     protected $name;
 
     protected $order;
 
-    protected $question_id;
-
     protected $type;
 
-    public function __construct( $id, $name, $type, $order, $question_id )
+    public function __construct( $id, $name, $type, $order, $exercise_id )
     {
         $this->id          = $id;
         $this->name        = $name;
         $this->type        = $type;
         $this->order       = $order;
-        $this->exercise_id = $question_id;
+        $this->exercise_id = $exercise_id;
     }
 
     public static function getAll( $exercise_id )
@@ -36,15 +36,35 @@ class Question
         $pdo                = getConnector();
         $query              = 'SELECT * FROM questions WHERE exercise_id=?';
         $stmt               = $pdo->prepare( $query );
-        $stmt->execute();
-        $questions = $stmt->fetchAll( $exercise_id );
+        $stmt->execute( [$exercise_id] );
+        $questions = $stmt->fetchAll();
 
         foreach ( $questions as $question )
         {
-            $questionsAsObject = new Question( $question['id'], $question['title'], $question['creation_date'], $question['modification_date'], $question['status'] );
+            $questionsAsObject = new Question( $question['id'], $question['name'], $question['type'], $question['order'], $question['exercise_id'] );
             array_push( $questionsAsObjects, $questionsAsObject );
         }
 
         return $questionsAsObjects;
+    }
+
+    public function getExerciseId()
+    {
+        return $this->exercise_id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getType()
+    {
+        return $this->type;
     }
 }
