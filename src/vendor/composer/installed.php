0e3a5304dabbdd1e846d7554b143bc740c6414b3
LPOdev <lpodevco@gmail.com>
2022-10-31 11:30:02 +0100

    vendor

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => 'a1558a189b2ff5c1e48a237b89b170ab6a767e86',
+        'reference' => 'c61246c82be2acdbdf379179b7223e6c1c56a3e7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => 'a1558a189b2ff5c1e48a237b89b170ab6a767e86',
+            'reference' => 'c61246c82be2acdbdf379179b7223e6c1c56a3e7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
