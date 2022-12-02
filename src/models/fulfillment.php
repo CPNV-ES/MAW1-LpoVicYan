d53667776be844d2f9db6deeebb444d102822005
Yann Menoud <Yann.MENOUD@cpnv.ch>
2022-12-02 11:57:37 +0100

    Create fulfillment model

--- src/models/fulfillment.php
+++ src/models/fulfillment.php
@@ -15,14 +15,32 @@ class Fulfillment
     /**
      * Get a fulfillment
      */
-    static function getAFulfillment( $id )
+    static function getAFulfillment($id)
     {
         $pdo = getConnector();
         $query = 'SELECT * FROM fulfillments WHERE id = ?';
-        $stmt = $pdo->prepare( $query );
-        $stmt->execute( [$id] );
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$id]);
         $fulfillment = $stmt->fetch();
-        $fulfillmentAsObject = new Fulfillment( $fulfillment['id'], $fulfillment['answer_id'] );
+        $fulfillmentAsObject = new Fulfillment($fulfillment['id'], $fulfillment['answer_id']);
         return $fulfillmentAsObject;
+    }
+
+    /**
+     * Create a fulfillment
+     */
+    function create()
+    {
+        $pdo = getConnector();
+        $query = 'SELECT * FROM fulfillments WHERE id = ?';
+        $stmt  = $pdo->prepare($query);
+        $stmt->execute([$this->id]);
+
+        if ($stmt->fetch() == null) {
+            $query = 'INSERT INTO fulfillments (answer_id) VALUES (?)';
+            $stmt  = $pdo->prepare($query);
+            $stmt->execute([$this->answer_id]);
+            return $pdo->lastInsertId();
+        }
     }
 }
